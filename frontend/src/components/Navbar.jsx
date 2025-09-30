a76ebaa08d62b706bc9264605b8030b1ae703484
DhirajIngale <idhiraj2204@gmai.com>
2025-09-30 23:16:09 +0530

    Complete Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
                   <p onClick={() => setToken(false)} className="hover:text-primary cursor-pointer">Logout</p>
                 </div>
               </div>
-              
+
             </div>
 
           : <button onClick={() => navigate('/login')} className="bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block hover:bg-primary/90 transition">
